3d5bd917d9bb0f9bdb58fdc12b78d43eeb1c4ca1
coderc <shao.c.coding@gmail.com>
2023-07-14 01:47:00 +0800

    update

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -3,15 +3,17 @@ package logger
 import (
 	"testing"
 
-	configStruct "github.com/coderc/onlinedisk-util/config_struct"
+	configStruct "github.com/coderc/onlinedisk-util/config"
 )
 
 func TestLogger(t *testing.T) {
-	loggerConfig := configStruct.LoggerConfig{
-		Level: "info",
-		Path:  "./test_data/test.log",
+	loggerConfig := configStruct.Config{
+		LoggerConfig: configStruct.LoggerConfigStruct{
+			Level: "debug",
+			Path:  "./test_data/logs.log",
+		},
 	}
-	GetLogger().Init(&loggerConfig)
+	GetLogger().Init(&loggerConfig.LoggerConfig)
 	GetLogger().Debug("debug info")
 	GetLogger().Info("info info")
 }
