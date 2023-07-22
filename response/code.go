98087db8a891d2664d9911ebe194b16eecdacea4
coderc <shao.c.coding@gmail.com>
2023-07-22 18:35:29 +0800

    update

---
+++ response/code.go
@@ -0,0 +1,15 @@
+package response
+
+const (
+	SccessCode               = 0
+	ErrorUsernameInvalidCode = 1001
+	ErrorPasswordInvalidCode = 1002
+)
+
+const (
+	ErrorGetUserInfo          = "get user info failed"
+	ErrorUsernameInvalid      = "username invalid"
+	ErrorPasswordInvalid      = "password invalid"
+	ErrorCreateUUIDFailed     = "create uuid failed"
+	ErrorInsertUserInfoFailed = "insert user info failed"
+)
